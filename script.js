7c51ce53e17ee02f50a5b6b699a3f88d6a460e3f
MarekCZFM <89865416+MarekCZFM@users.noreply.github.com>
2024-07-30 02:11:29 +0200

    Pridani funkce na polozeni kamene

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ let boxy = document.querySelectorAll('.pole');
 let hratZnovu = document.getElementsByClassName('hrat-znovu');
 let hlavniPole = document.querySelector('.hraci-plocha');
 
-let kolo = 0;
+let kolo = 0; // X == 0, Y == 1
+let odehraneHry = 1;
 
 function HratZnovu() {
     boxy.forEach(box => {
         box.innerHTML = "";
     });
-    kolo++;
+    kolo = 0;
+    odehraneHry++;
 }
 
+function PolozKostku(event) {
+    let pole = event.target;
+    if (pole.innerHTML === '')
+    {
+        kolo % 2 ? pole.innerHTML = 'X' : pole.innerHTML = 'Y';
+        kolo++;
+    }
+}
 HratZnovu();
