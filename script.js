5b40d2a945e99f34d8bdb013edc56cb0ebc56e5c
MarekCZFM <89865416+MarekCZFM@users.noreply.github.com>
2024-07-31 03:46:46 +0200

    Opraveni na pokladani kamenu X a O, ne X a Y a umozneni pokladani kamenu

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let boxy = document.querySelectorAll('.pole');
 let hratZnovu = document.getElementsByClassName('hrat-znovu');
 let hlavniPole = document.querySelector('.hraci-plocha');
 
+boxy.forEach(box => {
+    box.addEventListener('click', PolozKamen);
+})
+
 let kolo = 0; // X == 0, Y == 1
 let odehraneHry = 1;
 
@@ -17,7 +21,7 @@ function PolozKamen(event) {
     let pole = event.target;
     if (pole.innerHTML === '')
     {
-        kolo % 2 ? pole.innerHTML = 'X' : pole.innerHTML = 'Y';
+        kolo % 2 ? pole.innerHTML = 'X' : pole.innerHTML = 'O';
         kolo++;
     }
 }
